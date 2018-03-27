ed777f522678f5a5af4d1c4bcd002b829817fe46
bambutalid <37354757+bambutalid@users.noreply.github.com>
2018-03-27 11:06:48 +0800

    Part 1 refactoring for Tennis Game

--- main/TennisGame.java
+++ main/TennisGame.java
@@ -1,22 +1,22 @@
 import java.util.HashMap;
 
 public class TennisGame implements ScoreBoard {
+
     private static final String DEUCE = "Deuce";
     private static final String ADVANTAGE ="Advantage" ;
     private String button;
-    public int playerOneScore=0;
-    public int playerTwoScore=0;
-    HashMap runningScore = new HashMap();
+    int playerOneScore=0;
+    int playerTwoScore=0;
+    private HashMap<Integer, String> runningScore = new HashMap<>();
     private Player player;
     private boolean deuceStarted = false;
 
-    public TennisGame() {
+    TennisGame() {
         initializeRunningScores();
 
     }
 
     private void initializeRunningScores() {
-        runningScore = new HashMap();
         runningScore.put(1,"Love");
         runningScore.put(2, "Fifteen");
         runningScore.put(3, "Thirty");
@@ -50,14 +50,14 @@ public class TennisGame implements ScoreBoard {
 
     @Override
     public String getPlayerTotalScore() {
-        if(checkDeuceGame()){
-            return DEUCE;
-        }else if(advantageGame()){
-            return ADVANTAGE;
+        if(checkDeuceGame()) return DEUCE;
+        else if(advantageGame()) return ADVANTAGE;
+        else return getRunningScoreDisplay();
 
-        }else{
-            return (runningScore.get(playerOneScore)+" "+runningScore.get(playerTwoScore));
-        }
+    }
+
+    private String getRunningScoreDisplay() {
+        return runningScore.get(playerOneScore)+" "+runningScore.get(playerTwoScore);
     }
 
 
@@ -70,11 +70,16 @@ public class TennisGame implements ScoreBoard {
     }
 
     private boolean checkDeuceGame() {
-        if((playerOneScore >= 3) && (playerOneScore == playerTwoScore)){
-            deuceStarted = true;
-            return true;
-            }
+        if(checkPlayersScoreIsThreeOrGreater() && checkPlayersHaveSameScore()) return deuceStarted = true;
         return false;
+    }
+
+    private boolean checkPlayersScoreIsThreeOrGreater() {
+        return playerOneScore >= 3;
+    }
+
+    private boolean checkPlayersHaveSameScore() {
+        return playerOneScore == playerTwoScore;
     }
 
 
@@ -88,6 +93,6 @@ public class TennisGame implements ScoreBoard {
     public String getWinner() {
         if (playerOneScore > 3 && (playerOneScore >= (playerTwoScore + 2))) return "Win for player1";
 
-        return null;
+        return "Win for player2";
     }
 }
